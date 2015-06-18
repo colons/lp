2527c9450bf1296f3f796a6854ce90c59575aa92
colons <code@colons.co>
2015-06-18 22:19:34 +0100

    Be 0.5.2.
    We've changed web framework but things seem stable.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/colons/lp',
     author='colons',
     author_email='pypi@colons.co',
-    version='0.5.1',
+    version='0.5.2',
     license='LICENSE',
     platforms=['any'],
     packages=['lp'],

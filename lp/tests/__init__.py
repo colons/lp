51f28aa0fa4320ed744b5c9994a030eb9f57267c
colons <code@colons.co>
2015-06-28 09:35:55 +0100

    Put image testing code into its own function.
    We're gonna need to use this a bunch more when we start testing themes.

--- lp/tests/__init__.py
+++ lp/tests/__init__.py
@@ -1,5 +1,5 @@
 import os
-from tempfile import mkdtemp
+from tempfile import mktemp
 from unittest import TestCase
 
 from PIL import Image
@@ -19,26 +19,22 @@ class LPTest(TestCase):
             letters, ownership = os.path.splitext(filename)[0].split('_')
             yield letters, ownership, os.path.join(IMAGE_DIR, filename)
 
+    def assert_image_matches(self, letters, ownership, name):
+        grid = Grid.from_image(open(name))
+        self.assertEqual(
+            [(t.letter, t.ownership) for t in grid.tiles],
+            zip(letters, ownership)
+        )
+
     def test_examples_png(self):
-        for letters, ownership, name in self.pngs():
-            grid = Grid.from_image(open(name))
-            self.assertEqual(
-                [(t.letter, t.ownership) for t in grid.tiles],
-                zip(letters, ownership)
-            )
+        for png in self.pngs():
+            self.assert_image_matches(*png)
 
     def test_examples_jpg(self):
         # iOS transcodes pngs that it uploads to websites, including
         # screenshots, so we need to make sure we can withstand that.
 
-        jpg_dir = mkdtemp()
         for letters, ownership, png_path in self.pngs():
-            jpg_path = os.path.join(
-                jpg_dir, '{}_{}.jpg'.format(letters, ownership)
-            )
+            jpg_path = mktemp('.jpg')
             Image.open(png_path).save(jpg_path, format='JPEG', quality=80)
-            grid = Grid.from_image(open(jpg_path))
-            self.assertEqual(
-                zip(letters, ownership),
-                [(t.letter, t.ownership) for t in grid.tiles],
-            )
+            self.assert_image_matches(letters, ownership, jpg_path)
